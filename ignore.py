64db615993aa630a69115ecb2ed9b3de10740b84
Kiseon Jeong <glenjeong6418@gmail.com>
2020-09-30 01:01:16 +0900

    Revert "add tutorial codes"

--- ignore.py
+++
@@ -1,5 +0,0 @@
-# For tensorflow version 1.x
-import warnings
-warnings.filterwarnings('ignore', category=Warning)
-#warnings.filterwarnings('ignore', category=DeprecationWarning)
-#warnings.filterwarnings('ignore', category=FutureWarning)
