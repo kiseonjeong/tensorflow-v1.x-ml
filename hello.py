1369c0cf1e6ba804894ff53e6bf9f18cd2ac2a8e
Kiseon Jeong <glenjeong6418@gmail.com>
2020-09-30 23:35:42 +0900

    add deleted files

---
+++ hello.py
@@ -0,0 +1,17 @@
+import ignore
+import tensorflow as tf
+
+hello = tf.constant('Hello Tensorflow!')
+print(hello)
+
+a = tf.constant(10)
+b = tf.constant(32)
+c = tf.add(a, b)
+print(c)
+
+sess = tf.Session()
+
+print(sess.run(hello))
+print(sess.run([a, b, c]))
+
+sess.close()
